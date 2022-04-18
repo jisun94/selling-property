b5c75f2d021c3803d270a55d12c116892d9def35
jisun94 <jisun94lee@gmail.com>
2022-04-18 23:51:11 +0900

    ✨ Pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Signin />} />
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
     </>
   );
 }
