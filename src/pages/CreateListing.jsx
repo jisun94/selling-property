bed99ce1749763436ab8d8e52fb6f83f5d0745bb
jisun94 <jisun94lee@gmail.com>
2022-05-18 14:25:37 +0900

    🚧 Uploading images to firebase

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -13,7 +13,7 @@ import { v4 as uuidv4 } from "uuid";
 import Spinner from "../components/Spinner";
 
 function CreateListing() {
-  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [geolocationEnabled, setGeolocationEnabled] = useState(false);
   const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
@@ -82,32 +82,80 @@ function CreateListing() {
       return;
     }
 
-    let geolocation = {};
-    let location;
-    if (geolocationEnabled) {
-      const response = await fetch(`
-      https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=`);
+    // let geolocation = {};
+    // let location;
+    // if (geolocationEnabled) {
+    //   const response = await fetch(`
+    //   https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=`);
 
-      const data = await response.json();
+    //   const data = await response.json();
 
-      geolocation.lat = data.results[0]?.getometry.location.lat ?? 0;
-      geolocation.lng = data.results[0]?.getometry.location.lng ?? 0;
+    //   geolocation.lat = data.results[0]?.getometry.location.lat ?? 0;
+    //   geolocation.lng = data.results[0]?.getometry.location.lng ?? 0;
 
-      location =
-        data.status === "ZERO_RESULT"
-          ? undefined
-          : data.result[0]?.formatted_address;
+    //   location =
+    //     data.status === "ZERO_RESULT"
+    //       ? undefined
+    //       : data.result[0]?.formatted_address;
 
-      if (location === undefined || location.includes("undefined")) {
-        setLoading(false);
-        toast.error("Please enter a correct address");
-        return;
-      }
-    } else {
-      geolocation.lat = latitude;
-      geolocation.lng = longitude;
-      location = address;
-    }
+    //   if (location === undefined || location.includes("undefined")) {
+    //     setLoading(false);
+    //     toast.error("Please enter a correct address");
+    //     return;
+    //   }
+    // } else {
+    //   geolocation.lat = latitude;
+    //   geolocation.lng = longitude;
+    //   location = address;
+    // }
+
+    // Store image in firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, "images/" + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("Upload is " + progress + "% done");
+            switch (snapshot.state) {
+              case "paused":
+                console.log("Upload is paused");
+                break;
+              case "running":
+                console.log("Upload is running");
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error("Images are not uploaded");
+      return;
+    });
+    console.log(imgUrls);
 
     setLoading(false);
   };
