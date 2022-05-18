85289a776c75b93bc5c0b77de88b4155f063a7ec
jisun94 <jisun94lee@gmail.com>
2022-05-18 18:17:28 +0900

    ✨ Contact landlord page

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+
+function Contact() {
+  const [message, setMessage] = useState("");
+  const [landlord, setLandlord] = useState(null);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const params = useParams();
+
+  useEffect(() => {
+    const getLandlord = async () => {
+      const docRef = doc(db, "users", params.landlordId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        setLandlord(docSnap.data());
+      } else {
+        toast.error("Could not get landlord data");
+      }
+    };
+
+    getLandlord();
+  }, [params.landlordId]);
+
+  const onChange = (e) => setMessage(e.target.value);
+  return (
+    <div className="page-container">
+      <header>
+        <p className="page-header">Contact Landlord</p>
+      </header>
+
+      {landlord !== null && (
+        <main>
+          <div className="contact-landlord">
+            <p className="landlord-name">Contact {landlord?.name}</p>
+          </div>
+
+          <form className="message-form">
+            <div className="message-div">
+              <label className="message-label" htmlFor="message">
+                Message
+              </label>
+              <textarea
+                className="textarea"
+                id="message"
+                value={message}
+                onChange={onChange}
+                name="mesage"
+              ></textarea>
+            </div>
+
+            <a
+              href={`mailto:${landlord.email}?Subject=${searchParams.get(
+                "listingName"
+              )}&body=${message}`}
+            >
+              <button type="button" className="primary-button">
+                Send Message
+              </button>
+            </a>
+          </form>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default Contact;
