f8d3ce9ad9348be965db510749b5547020b78650
jisun94 <jisun94lee@gmail.com>
2022-05-22 21:04:56 +0900

    💄 Add Leaflet map

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -12,7 +12,7 @@ function ListingItem({ listing, id, onDelete }) {
       >
         <img
           className="category-listing-img"
-          src={listing.imageUrls[0]}
+          src={listing.imgUrls[0]}
           alt={listing.name}
         />
         <div className="category-listing-details">
