78e4a2921c1837af59d4a9426ac64773cd6d8722
jisun94 <jisun94lee@gmail.com>
2022-05-23 10:24:35 +0900

    🚧 Edit Listing

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -95,6 +95,7 @@ function Profile() {
     }
   };
 
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`);
   return (
     <div className="profile">
       <header className="profile-header">
@@ -158,6 +159,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
